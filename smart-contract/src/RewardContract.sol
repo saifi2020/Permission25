237afb855b12ea8da1111b846e1f796bf7237c28
AdnanSlef <aself.school@gmail.com>
2025-06-22 14:28:53 -0400

    make pseudocode contract into one that compiles

--- smart-contract/src/RewardContract.sol
+++ smart-contract/src/RewardContract.sol
@@ -1,22 +1,24 @@
+pragma solidity 0.8.29;
+
 contract RewardContract {
     uint256 rewardValidationKey;
     constructor(uint256 _rewardValidationKey) {
-        rewardValidationKey = _rewardValidationKey
-    };
+        rewardValidationKey = _rewardValidationKey;
+    }
 
     function claimRewards(uint256 claim) public {
         // check if msg.sender is owed some rewards
-        assert _isValidClaim(claim);
+        require(_isValidClaim(claim), "Invalid Claim");
         // if so, give rewards
-    };
+    }
     function setRewards(uint256 rewards) public {
-        assert _isValid(rewards);
-    };
+        require(_isValidRewards(rewards), "Invalid Rewards");
+    }
     
     function _isValidRewards(uint256 rewards) internal returns (bool) {
         // Check if the rewards are accurate
         // Using rewardValidationKey
-    };
+    }
     function _isValidClaim(uint256 claim) internal returns (bool) {
         // Checks claim
     }
