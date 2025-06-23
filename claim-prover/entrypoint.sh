930b9c0780a4802b73fc319d5fc6a17bba2003f5
AdnanSlef <aself.school@gmail.com>
2025-06-23 17:21:42 -0400

    create full noir circuit; limit it to verifying in claim-prover since the proving will happen client-side in the browser

--- claim-prover/entrypoint.sh
+++ claim-prover/entrypoint.sh
@@ -3,23 +3,17 @@ set -e
 
 cd noir_circuits
 
+# Only build the verifier circuits; don't do any proving here.
+# Clients will prove browser-side.
+
 # build-circuit:
 nargo build
-
-# execute-circuit:
-nargo execute
 
 # compile-circuit: 
 nargo compile
 
-# prove-circuit:
-bb prove -b ./target/noir_circuits.json -w ./target/noir_circuits.gz -o ./target --oracle_hash keccak
-
 # generate-vk:
 bb write_vk -b ./target/noir_circuits.json -o ./target --oracle_hash keccak
-
-# verify-proof: 
-bb verify -k ./target/vk -p ./target/proof --oracle_hash keccak
 
 # generate-solidity-verifier:
 bb write_solidity_verifier -k ./target/vk -o ./target/Verifier.sol
